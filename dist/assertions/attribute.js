67adbf81a6728fa06178dd7e4ad5aa7bbcfa5cf6
Vitalii Grygoruk <vitalii@peakon.com>
2019-11-14 13:46:12 +0100

    build

---
+++ dist/assertions/attribute.js
@@ -0,0 +1,51 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = attribute;
+
+var _defaultConfig = require('../util/default-config');
+
+var _defaultConfig2 = _interopRequireDefault(_defaultConfig);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var doesOneElementHaveAttribute = function doesOneElementHaveAttribute(client, selector, attribute, expected) {
+    var elements = client.$$(selector);
+    var values = [];
+    var filteredList = elements.filter(function (element) {
+        var value = element.getAttribute(attribute);
+        values.push(value);
+        var elementHasExpectedValue = expected instanceof RegExp ? value.match(expected) : value === expected;
+
+        return elementHasExpectedValue;
+    });
+
+    return {
+        result: filteredList.length > 0,
+        values: values
+    };
+};
+
+function attribute(client, chai, utils, options) {
+    var config = (0, _defaultConfig2.default)(options);
+    chai.Assertion.addMethod('attribute', function (attribute, expected) {
+        var selector = utils.flag(this, 'object');
+        var negate = utils.flag(this, 'negate');
+        var immediately = utils.flag(this, 'immediately');
+
+        if (!immediately) {
+            try {
+                client.waitUntil(function () {
+                    return doesOneElementHaveAttribute(client, selector, attribute, expected).result === !negate;
+                }, config.defaultWait);
+            } catch (e) {
+                // actual assertion is handled below
+            }
+        }
+
+        var elementContainsAttribute = doesOneElementHaveAttribute(client, selector, attribute, expected);
+        this.assert(elementContainsAttribute.result, 'Expected an element matching <' + selector + '> to contain attribute "' + attribute + '" with value "' + expected + '", but only found these values: ' + elementContainsAttribute.values, 'Expected an element matching <' + selector + '> not to contain attribute "' + attribute + ' with value "' + expected + '", but found these attributes: ' + elementContainsAttribute.values);
+    });
+}
